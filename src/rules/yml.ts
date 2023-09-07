3c63dd2a0b4c2c0fa3ea1a09daa3e16257241280
Rostyslav Miniukov <miniukovrostyslav@gmail.com>
2023-09-07 14:19:10 +0300

    fix(rules): 🐛 yml curly spacing rule to match prettier config

--- src/rules/yml.ts
+++ src/rules/yml.ts
@@ -2,6 +2,7 @@ import { defineRules } from '../helpers/utilities';
 
 export const ymlRules = defineRules({
   'yml/quotes': ['error', { prefer: 'single', avoidEscape: false }],
+  'yml/flow-mapping-curly-spacing': ['error', 'always'],
   'yml/no-empty-document': 'off',
   'yml/no-empty-mapping-value': 'off',
 });
