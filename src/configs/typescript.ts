9b53a5103ca278845a9e92a4a8b2865593818733
Rostyslav Miniukov <miniukovrostyslav@gmail.com>
2024-05-31 21:17:26 +0300

    fix(config): 🐛 ⚙️ added prettier plugin to typescript config

--- src/configs/typescript.ts
+++ src/configs/typescript.ts
@@ -47,6 +47,7 @@ export const typescriptConfig = defineConfig({
          */
         'plugin:@typescript-eslint/recommended',
         'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:prettier/recommended',
       ],
       parser: '@typescript-eslint/parser',
       parserOptions: {
